e6eee3150ff85ac9b1c13803ce8c70e35704e7a0
Yinan Fang <yinan_fang@hotmail.com>
2015-12-23 08:57:22 +0800

    Initial Commit

---
+++ iOS-9-TemplateTests/iOS_9_TemplateTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_9_TemplateTests.swift
+//  iOS-9-TemplateTests
+//
+//  Created by Yinan Fang on 12/23/15.
+//  Copyright © 2015 Guru. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_9_Template
+
+class iOS_9_TemplateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
